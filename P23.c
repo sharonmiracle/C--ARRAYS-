4340982cac121a4599412f61f3032a08dcb38928
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2025-01-24 16:16:52 +0530

    Create P23.c

---
+++ P23.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int sum=0;
+    printf("Enter no. of array elements : ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        int flag=1;
+        if(arr[i]<=1)
+        {
+            flag=0;
+        }
+        for(int j=2;j<=arr[i]/2;j++)
+        {
+            if(arr[i]%j==0)
+            {
+                flag=0;
+                break;
+            }
+        }
+        if(flag)
+        {
+            sum+=arr[i];
+        }
+    }
+    printf("The sum of Prime Numbers is : %d",sum);
+    return 0;
+}
