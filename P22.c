f5893585b303077efc8613d2c793656cb10fde1f
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2025-01-24 15:57:28 +0530

    Create P22.c

---
+++ P22.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter no. of array elements : ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("The Non Prime Numbers are : ");
+    for(int i=0;i<n;i++)
+    {
+        int flag=0;
+        if(arr[i]<=1)
+        {
+            flag=0;
+        }
+        for(int j=2;j<=arr[i]/2;j++)
+        {
+            if(arr[i]%j==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    return 0;
+}
